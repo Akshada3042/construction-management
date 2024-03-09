d72b8f9d844c9b3ac4c4a0884bae2cd18c952093
Akshada3042 <akshadakale33@gmail.com>
2024-03-09 12:22:18 +0530

    modified git repo

--- src/VedanandConstruction/src/main/java/com/example/vedanandConstruction/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.vedanandConstruction.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.vedanandConstruction.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
--- src/VedanandConstruction/src/main/java/com/example/vedanandConstruction/repository/ProjectStageRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.vedanandConstruction.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.vedanandConstruction.entity.ProjectStages;
-
-public interface ProjectStageRepo extends JpaRepository<ProjectStages,Integer>{
-
-}
