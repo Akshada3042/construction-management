c59648d6d4d80192327537ace162efa6bb8c8038
Akshada3042 <akshadakale33@gmail.com>
2024-03-09 13:02:44 +0530

    made chages in src frontend

--- construction/src/VedanandConstruction/src/main/java/com/example/vedanandConstruction/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.vedanandConstruction.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.vedanandConstruction.entity.Jobs;
-
-public interface JobRepository extends JpaRepository<Jobs, Integer>{
-
-}
--- construction/src/VedanandConstruction/src/main/java/com/example/vedanandConstruction/repository/MaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.vedanandConstruction.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.vedanandConstruction.entity.Material;
-
-public interface MaterialRepository extends JpaRepository<Material, Integer> {
-
-}
